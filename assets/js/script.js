2d6f9dc4dc59d8ec32b91f4f2caf3b3d39812a3d
Nazia R <nazia.huma@gmail.com>
2021-04-15 19:30:00 +0100

    added on submit event to the form

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const onSubmit = (event) => {
+  event.preventDefault();
+  const location = $("#location-input").val();
+  const interest = $("#interest-input").val();
+
+  
+};
+
+$("#search-form").on("submit", onSubmit);
+
+$(document).ready();
